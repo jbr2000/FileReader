8549b9c270b0f2c6ffa1ee82d1890ed08abb17ed
jbr2000 <jbrupert@mail.csuchico.edu>
2019-10-02 16:19:17 -0700

    gives metadata of file

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main()
   string fileName; //to get the name of the file to open
   string line; //to store a single line of a text file
   fstream fileStream; //to open a file for reading
+  int characters=0;
+  int lineNum=0;
 
   cout<<"What file do you want to open? ";
   getline(cin, fileName);
@@ -17,29 +19,36 @@ int main()
   
   if(fileStream.is_open()) /*STEP 2: check to see if the fileStream successfully opened*/ 
   {
-    cout<<fileName<<" opened.\nFILE CONTENTS:\n";
+   
+          cout<<fileName<<" opened.\nFILE CONTENTS:\n";
     
     //STEP 3: repeat the following until the end-of-file (eof) has been reached...  
-
+  
    while(fileStream.eof() == false)
    { 
     getline (fileStream, line);
+  if (!line.empty()){         
  cout<<line<<endl;
+ lineNum = lineNum + 1;
+ characters = characters + line.length();
+  }
+
    }
-         
- 
-   
-   
+          
           // 3A: read a line from fileStream into the variable line
     // 3B: display the line, followed by an endline
-  
+   
     //STEP 4: close the fileStream
   
   }
   else
   {
     cout<<fileName<<" could not be opened.\n";
-  }
+  
+ }
+cout<<"METADATA"<<endl<<"File: "<<fileName<<endl<<"Lines: "<<lineNum<<endl<<"Characters: "<<characters<<endl;
+
+  
 fileStream.close();
   return 0;
 }
